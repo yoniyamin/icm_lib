4416a878b1ac30538da48cf84025dc44ef6fcda1
yoniyamin <144470785+yoniyamin@users.noreply.github.com>
2025-01-15 22:02:15 +0100

    remove map

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: "/icm_lib/",
-  build: {
-    sourcemap: true, // Disable source maps
-  },
   server: {
     port: 3000,
     open: true,

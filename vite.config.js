50095915e99255d26fd30cb3a53064b833d7c81d
yoniyamin <144470785+yoniyamin@users.noreply.github.com>
2025-01-15 16:12:28 +0100

    changed base again

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/icm_lib/src",
+  base: "/icm_lib/",
   build: {
     sourcemap: true, // Disable source maps
   },

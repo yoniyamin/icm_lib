2952d62f899d3732f29d07eb45d7504c409b3a49
yoniyamin <144470785+yoniyamin@users.noreply.github.com>
2025-01-15 15:24:47 +0100

    change url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/icm_lib",
+  base: "/icm_lib/src",
   build: {
     sourcemap: true, // Disable source maps
   },

b2c0013776f4bd9f85f60fe4a4d0ca4ed4995f7e
yoniyamin <144470785+yoniyamin@users.noreply.github.com>
2025-01-17 13:16:43 +0100

    adding authentication headers and tokens

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,6 +1,6 @@
 // API CONFIGURATION FILE
 
-// const BASE_URL = "https://religious-cherianne-dbprops-ef8ee171.koyeb.app";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://religious-cherianne-dbprops-ef8ee171.koyeb.app";
+//const BASE_URL = "http://localhost:5000";
 
 export default BASE_URL;

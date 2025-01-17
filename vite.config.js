0b21b773490cf5c68d665d21614426f449e19c9a
yoniyamin <144470785+yoniyamin@users.noreply.github.com>
2025-01-17 18:16:55 +0100

    changing dropdown placeholder

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Split vendor libraries
+        },
+      },
+    },
+  },
+  chunkSizeWarningLimit: 1000, // Adjust size limit if needed
   base: "/icm_lib/",
   server: {
     port: 3000,
